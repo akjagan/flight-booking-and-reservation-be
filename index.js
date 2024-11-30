e06079818e67f94e0fb2e5e2b3861dbc9d0cd03d
GanishwarArun <ganishwarips@gmail.com>
2024-11-30 22:11:07 +0530

    cors  update

--- index.js
+++ index.js
@@ -35,17 +35,14 @@ const allowedOrigins = [
   "http://localhost:5174",
 ]; // Your frontend URL(s)
 
-const options = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-}
-  
-app.use(cors(options));
+const corsOptions = {
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+
 // Middleware
 app.use(logger("dev"));
 app.use(express.json());
